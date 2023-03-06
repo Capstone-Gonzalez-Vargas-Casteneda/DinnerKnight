87ed2655f8e0bea24c20add43672c705d7b1eeea
Matthew <gonzalez.matthew.steven@gmail.com>
2023-03-06 15:24:54 -0500

    first push

---
+++ src/main/java/com/example/dinnerknight/DinnerKnightApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinnerknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerKnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinnerKnightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dinnerknight/DinnerKnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinnerknight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerKnightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
