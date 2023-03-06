dd7af20d8b3233079248435267477462d54137ee
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-03-06 16:29:24 -0600

    model user

--- src/main/java/com/example/dinnerknight/models/User.java
+++ src/main/java/com/example/dinnerknight/models/User.java
@@ -1,4 +1,126 @@
 package com.example.dinnerknight.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isCook;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Food> foods;
+
+    public User(){
+    }
+
+    public User(User copy){
+        id = copy.id;
+        email = copy.email;
+        username = copy.username;
+        first_name = copy.first_name;
+        last_name = copy.last_name;
+        password = copy.password;
+        isCook = copy.isCook;
+    }
+
+    public User(long id, String email, String username, String first_name, String last_name, String password, boolean isCook, List<Food> foods) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.password = password;
+        this.isCook = isCook;
+        this.foods = foods;
+    }
+    public User(long id, String username, boolean isCook, List<Food> foods) {
+        this.id = id;
+        this.username = username;
+        this.isCook = isCook;
+        this.foods = foods;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isCook() {
+        return isCook;
+    }
+
+    public void setCook(boolean cook) {
+        isCook = cook;
+    }
+
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<Food> foods) {
+        this.foods = foods;
+    }
 }
