0d5cf9a5a4338db4868010437cd75fbeaedad148
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-03-15 11:06:14 -0500

    landing page

--- src/main/java/com/example/dinnerknight/models/Food.java
+++ src/main/java/com/example/dinnerknight/models/Food.java
@@ -35,7 +35,7 @@ public class Food {
     public Food(Food copy) {
         name = copy.name;
         ingredients = copy.ingredients;
-        measurements = copy.measurements;
+//        measurements = copy.measurements;
         instructions = copy.instructions;
     }
 
@@ -43,7 +43,7 @@ public class Food {
         this.id = id;
         this.name = name;
         this.ingredients = ingredients;
-        this.measurements = measurements;
+//        this.measurements = measurements;
         this.instructions = instructions;
     }
 
@@ -84,13 +84,13 @@ public class Food {
     public void setInstructions(String instructions) {
         this.instructions = instructions;
     }
-    public String getMeasurements() {
-        return measurements;
-    }
-
-    public void setMeasurements(String measurements) {
-        this.measurements = measurements;
-    }
+//    public String getMeasurements() {
+//        return measurements;
+//    }
+//
+//    public void setMeasurements(String measurements) {
+//        this.measurements = measurements;
+//    }
 
 }
 
